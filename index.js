d62c5cee9d156b1b2ddf6c475cd3f57f84bc9a7f
sam <83267739+sammed-code@users.noreply.github.com>
2024-01-17 11:42:36 +0000

    5

--- index.js
+++ index.js
@@ -10,17 +10,18 @@ var htmlTaskContent = ({ id, title, description, type, url }) => `
    <div class='col-md-6 col-lg-4 mt-3' id=${id} key=${id}>
         <div class='card shadow-sm task__card'>
             <div class='card-header d-flex justify-content-end task__card__header'>
-              <button type="button" class="btn btn-outline-info mr-2" name="${id}">
+              <button type="button" class="btn btn-outline-info mr-2" name="${id}" onclick="editTask.apply(this, arguments)">
                 <i class="fas fa-pencil-alt" name=${id}></i>
               </button>
-               <button type="button" class="btn btn-outline-danger mr-2" name="${id}">
+               <button type="button" class="btn btn-outline-danger mr-2" name="${id}" onclick="deleteTask.apply(this, arguments)">
                 <i class="fas fa-trash-alt" name=${id}></i>
               </button>
             </div>
             <div class="card-body">
                ${
-                 url &&
-                 `<img width='100%' src=${url} alt='card image cap' class='card-img-top md-3 rounded-lg' />`
+                 url
+                   ? `<img width='100%' src=${url} alt='card image cap' class='card-img-top md-3 rounded-lg' />`
+                   : `<img width='100%' src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAMFBMVEXp7vG6vsG3u77s8fTCxsnn7O/f5OfFyczP09bM0dO8wMPk6ezY3eDd4uXR1tnJzdBvAX/cAAACVElEQVR4nO3b23KDIBRA0ShGU0n0//+2KmO94gWZ8Zxmr7fmwWEHJsJUHw8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAwO1MHHdn+L3rIoK6eshsNJ8kTaJI07fERPOO1Nc1vgQm2oiBTWJ+d8+CqV1heplLzMRNonED+4mg7L6p591FC+133/xCRNCtd3nL9BlxWP++MOaXFdEXFjZ7r8D9l45C8y6aG0cWtP/SUGhs2d8dA/ZfGgrzYX+TVqcTNRRO9l+fS5eSYzQs85psUcuzk6igcLoHPz2J8gvzWaH/JLS+95RfOD8o1p5CU5R7l5LkfKEp0mQ1UX7hsVXqDpRrifILD/3S9CfmlUQFhQfuFu0STTyJ8gsP3PH7GVxN1FC4t2sbBy4TNRTu7LyHJbqaqKFw+/Q0ncFloo7CjRPwMnCWqKXQZ75El4nKC9dmcJaou9AXOE5UXbi+RGeJygrz8Uf+GewSn9uXuplnWDZJ7d8f24F/s6iq0LYf9olbS3Q8i5oKrRu4S9ybwaQ/aCkqtP3I28QDgeoK7TBya/aXqL5COx67PTCD2grtdOwH+pQV2r0a7YVBgZoKwwIVFQYG6ikMDVRTGByopjD8ATcKb0UhhRTe77sKs2DV7FKSjId18TUEBYVyLhUThWfILHTDqmI85/2RWWjcE/bhP6OD7maT3h20MHsA47JC3PsW0wcwLhv9t0OOPOIkCn21y2bXXwlyylxiYMPk1SuCSmpfK8bNQvIrpAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwNX4BCbAju9/X67UAAAAASUVORK5CYII=' alt='card image cap' class='card-img-top md-3 rounded-lg' />`
                }
             <h4 class="card-title">${title}</h4>
             <p class="description trim-3-lines text-muted data-gram_editor='false'">${description}</p>
@@ -29,7 +30,7 @@ var htmlTaskContent = ({ id, title, description, type, url }) => `
             </div>
             </div>
             <div class="card-footer">
-              <button type="button" class="btn btn-outline-primary float-right" data-bs-toggle="modal" data-bs-target="#showTask">Open Task</button>
+              <button type="button" class="btn btn-outline-primary float-right" data-bs-toggle="modal" data-bs-target="#showTask" id=${id} onclick='openTask.apply(this, arguments)'>Open Task</button>
             </div>
         </div>
     </div>
@@ -40,8 +41,9 @@ var htmlModalContent = ({id, title, description, url}) => {
   return `
     <div id=${id}>
       ${
-        url &&
-        `<img width='100%' src=${url} alt='card image cap' class='card-img-top md-3 rounded-lg' />`
+        url
+          ? `<img width='100%' src=${url} alt='card image cap' class='card-img-top md-3 rounded-lg' />`
+          : `<img width='100%' src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAMFBMVEXp7vG6vsG3u77s8fTCxsnn7O/f5OfFyczP09bM0dO8wMPk6ezY3eDd4uXR1tnJzdBvAX/cAAACVElEQVR4nO3b23KDIBRA0ShGU0n0//+2KmO94gWZ8Zxmr7fmwWEHJsJUHw8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAwO1MHHdn+L3rIoK6eshsNJ8kTaJI07fERPOO1Nc1vgQm2oiBTWJ+d8+CqV1heplLzMRNonED+4mg7L6p591FC+133/xCRNCtd3nL9BlxWP++MOaXFdEXFjZ7r8D9l45C8y6aG0cWtP/SUGhs2d8dA/ZfGgrzYX+TVqcTNRRO9l+fS5eSYzQs85psUcuzk6igcLoHPz2J8gvzWaH/JLS+95RfOD8o1p5CU5R7l5LkfKEp0mQ1UX7hsVXqDpRrifILD/3S9CfmlUQFhQfuFu0STTyJ8gsP3PH7GVxN1FC4t2sbBy4TNRTu7LyHJbqaqKFw+/Q0ncFloo7CjRPwMnCWqKXQZ75El4nKC9dmcJaou9AXOE5UXbi+RGeJygrz8Uf+GewSn9uXuplnWDZJ7d8f24F/s6iq0LYf9olbS3Q8i5oKrRu4S9ybwaQ/aCkqtP3I28QDgeoK7TBya/aXqL5COx67PTCD2grtdOwH+pQV2r0a7YVBgZoKwwIVFQYG6ikMDVRTGByopjD8ATcKb0UhhRTe77sKs2DV7FKSjId18TUEBYVyLhUThWfILHTDqmI85/2RWWjcE/bhP6OD7maT3h20MHsA47JC3PsW0wcwLhv9t0OOPOIkCn21y2bXXwlyylxiYMPk1SuCSmpfK8bNQvIrpAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwNX4BCbAju9/X67UAAAAASUVORK5CYII=' alt='card image cap' class='card-img-top md-3 rounded-lg' />`
       }
       <strong class='text-sm text-muted'>Created on ${date.toDateString()}</strong>
       <h2 class='my-3'>${title}</h2>
@@ -57,7 +59,7 @@ var updateLocalStorage = () => {
 }
 
 var loadInitialData = () => {
-  var localStorageCopy = JSON.parse(localStorage.tasks);
+  var localStorageCopy = JSON.parse(localStorage.task);
   if (localStorageCopy) {
     state.taskList = localStorageCopy.tasks;
   }
@@ -74,4 +76,83 @@ var handleSubmit = (event) => {
     type: document.getElementById("tags").value,
     description: document.getElementById("taskDescription").value
   };
+
+  if (input.title === '' || input.type === '' || input.description === '') {
+    return alert("Please fill all the required fields");
+  }
+
+  // spread operator
+  taskContents.insertAdjacentHTML("beforeend", htmlTaskContent({
+    ...input, id
+  }))
+
+  state.taskList.push({...input, id});
+  updateLocalStorage();
+}
+
+var openTask = (e) => {
+  if(!e) e = window.event;
+
+  var getTask = state.taskList.find(({id}) => id === e.target.id);
+  taskModal.innerHTML = htmlModalContent(getTask);
+}
+
+var deleteTask = (e) => {
+  if (!e) e = window.event;
+
+  var targetID = e.target.getAttribute("name");
+  var type = e.target.tagName;
+  var removeTask = state.taskList.filter(({id}) => id !== targetID);
+
+  state.taskList = removeTask;
+  updateLocalStorage();
+
+  if (type === "BUTTON") {
+    return e.target.parentNode.parentNode.parentNode.parentNode.removeChild(
+      e.target.parentNode.parentNode.parentNode
+    );
+  }
+  return e.target.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(
+    e.target.parentNode.parentNode.parentNode.parentNode
+  );
+}
+
+var editTask = (e) => {
+  if (!e) e = window.event;
+
+  var targetID = e.target.id;
+  var type = e.target.tagName;
+
+  var parentNode;
+  let taskTitle;
+  var taskDescription;
+  var taskType;
+  let submitButton;
+
+  if (type === "BUTTON") {
+    parentNode = e.target.parentNode.parentNode;
+  }
+  else {
+    parentNode = e.target.parentNode.parentNode.parentNode;
+  }
+
+  taskTitle = parentNode.childNodes[3].childNodes[3];
+  taskDescription = parentNode.childNodes[3].childNodes[5];
+  taskType = parentNode.childNodes[3].childNodes[7].childNodes[1];
+  submitButton = parentNode.childNodes[5].childNodes[1];
+
+  taskTitle.setAttribute("contenteditable", "true");
+  taskDescription.setAttribute("contenteditable", "true");
+  taskType.setAttribute("contenteditable", "true");
+
+  // submitButton.setAttribute('onclick', "saveEdit.apply(this, arguments)");
+  submitButton.removeAttribute("data-bs-toggle");
+  submitButton.removeAttribute("data-bs-target");
+  submitButton.innerHTML = "Save Changes";
+}
+
+var saveEdit = (e) => {
+  if (!e) e = window.event;
+  
+  var targetID = e.target.id;
 }
